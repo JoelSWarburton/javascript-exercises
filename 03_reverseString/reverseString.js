4edbd31e8f43357043a9e38b80aa88ba7ed9cfa0
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-16 22:48:51 +1100

    Add code to reverse strings to pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+    newWord = word.split("");
+    return newWord.reverse().join("");
 
 };
 
+
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
