a39b60f469eeb8ded521cf2bc1d0935bea7c1734
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-16 23:18:50 +1100

    Implement code to remove args from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...toRemove) {
+    
+    for(const element of toRemove) {
+        removeIndex = theArray.indexOf(element);
+        
+        //skip if element not present in array
+        if (removeIndex == -1) continue;
 
+        theArray.splice(removeIndex, 1);
+    }
+    return theArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
