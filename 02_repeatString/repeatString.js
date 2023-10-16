8dc159e1dac26097b10ac383de53279953c1002a
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-16 16:49:53 +1100

    Add code to make repeatString.js pass tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, number) {
+      let  newString = "";
+    if (number < 0) return "ERROR";
 
+
+    for (let i = 0; i < number; i++) {
+        newString += str;
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
