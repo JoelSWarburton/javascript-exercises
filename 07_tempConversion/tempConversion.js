a0ebbcfe2299d2bf732f4d65c50167d80237fcbc
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-18 12:27:27 +1100

    Implement functions convertToCelsius and convertToFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  tempCelsius = (tempFahrenheit - 32) * (5/9);
+  tempCelsius = Math.round(tempCelsius * 10) / 10
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+
+  tempFahrenheit = tempCelsius * (9/5) + 32
+  tempFahrenheit = Math.round(tempFahrenheit * 10) / 10;
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
