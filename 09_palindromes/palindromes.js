d3815e6a531cb4532e1ddede465b39771eb54f6f
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-20 13:57:22 +1100

    Implement palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    
 
+    word = word.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let reversedWord = ""
+    for(i = word.length -1; i >= 0; i--) {
+        reversedWord += word[i]
+    }
+   
+    for(i = 0; i < word.length; i++) {
+        if(word[i] != reversedWord[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
