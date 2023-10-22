9a44a0197efa4eda5b902b67512b57ba093ae4d0
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-23 00:05:19 +1100

    Implement fibonacci code to pass test cases

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    
+    let first = 1;
+    let second = 1;
 
+    if (number < 0) {
+        return "OOPS"
+    }
+
+    //two numbers dont get added together and return 1;
+    if (number <= 2) {
+        return 1;
+    }
+ 
+    for(let i = 3; i <= number; i++) {
+        let temp = first + second;
+        first = second;
+        second = temp;
+    }
+
+    return second;
 };
 
 // Do not edit below this line
