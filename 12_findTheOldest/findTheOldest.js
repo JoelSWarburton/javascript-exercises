3390510649dfee726221b86e379067172a444181
Joel Warburton <Joel.s.warburton@gmail.com>
2023-10-23 12:09:50 +1100

    Implement findTheOldest function and helper function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people[0];
+    for(const person of people) {
 
+        if (age(person) > age(oldest)) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
+
+const age = function(person) {
+    
+    if(person.yearOfDeath == undefined) {
+        let currentYear = new Date().getFullYear();
+        
+        return currentYear - person.yearOfBirth;
+    }
+
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
